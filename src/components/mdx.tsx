9c0afaf64f1617bc4bf7055210a4a5616e78cae0
hbdrevv <vanderveendrew@gmail.com>
2026-07-28 14:40:45 -0400

    type table added

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,10 +1,12 @@
 
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    TypeTable,
     ...components,
   } satisfies MDXComponents;
 }
